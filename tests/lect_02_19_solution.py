57637cf6da5c9d7101ed797acd5e54da4db8c883
easyfly007 <easyfly.huang@gmail.com>
2018-03-03 11:07:11 +0800

    add 19 solution

--- tests/lect_02_19_solution.py
+++ tests/lect_02_19_solution.py
@@ -8,7 +8,7 @@
 # If a cell is a wall or it is impossible to reach the goal from a cell,
 # assign that cell a value of 99.
 # ----------
-
+import copy
 grid = [[0, 1, 0, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
@@ -24,11 +24,57 @@ delta = [[-1, 0 ], # go up
 
 delta_name = ['^', '<', 'v', '>']
 
+def get_prev_list(curr, value, delta):
+    x_range = list(range(len(value)))
+    y_range = list(range(len(value[0])))
+    prev_list = []
+    curr_x, curr_y = curr
+    for move in delta:
+        dx, dy = move
+        prev = [curr_x - dx, curr_y - dy]
+        if prev[0] in x_range and prev[1] in y_range:
+            if value[prev[0]][prev[1]] == -1:
+                prev_list.append(prev)
+    return prev_list
+
 def compute_value(grid,goal,cost):
     # ----------------------------------------
     # insert code below
     # ----------------------------------------
     
-    # make sure your function returns a grid of values as 
-    # demonstrated in the previous video.
-    return value 
+    # initialize
+    x_range, y_range = list(range(len(grid))), list(range(len(grid[0])))
+    value = [[-1,] * len(y_range) for i in x_range]
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                value[i][j] = 99
+    value[goal[0]][goal[1]] = 0
+
+    open = [goal,]
+    while len(open):
+        open.reverse()
+        curr = open.pop()
+        prev_list = get_prev_list(curr, value, delta)
+        open += prev_list
+        for prev in prev_list:
+            value[prev[0]][prev[1]] = value[curr[0]][curr[1]] + 1
+
+    movement = copy.deepcopy(grid)
+    for x1 in x_range:
+        for y1 in y_range:
+            min_val = 999
+            if grid[x1][y1] == 1:
+                continue
+            for k in range(len(delta)):
+                dx, dy = delta[k]
+                x2, y2 = x1 + dx, y1 + dy
+                if x2 in x_range and y2 in y_range:
+                    if value[x2][y2] < min_val :
+                        movement[x1][y1] = delta_name[k]
+                        min_val = value[x2][y2]
+    return movement
+
+value = compute_value(grid, goal, cost)
+for i in value:
+    print(i)
