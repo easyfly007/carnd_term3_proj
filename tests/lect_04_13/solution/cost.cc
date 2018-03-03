fdae2b7c698279ae5b59c3bac2a7ec095c8aad3b
easyfly007 <easyfly.huang@gmail.com>
2018-03-03 22:45:00 +0800

    add the 20 quiz

--- tests/lect_04_13/solution/cost.cc
+++ tests/lect_04_13/solution/cost.cc
@@ -12,6 +12,7 @@ float goal_distance_cost(int goal_lane, int intended_lane, int final_lane, float
     and the distance of the final lane from the goal. The cost of being out of the 
     goal lane also becomes larger as vehicle approaches the goal.
     */
-
-    return cost = 1.0 - exp(- abs(intended_lane - final_lane) / distance_to_goal);
+	int delta_d = 2.0*goal_lane - intended_lane - final_lane;
+    float cost = 1 - exp(-(abs(delta_d) / distance_to_goal));
+	return cost;    
 }
