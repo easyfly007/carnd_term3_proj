ce25f3ebe25ebf09d82d0ce57bd9de501a636453
easyfly007 <easyfly.huang@gmail.com>
2018-04-01 16:33:33 +0800

    add keep lane code

--- PathPlanningProject/src/main.cpp
+++ PathPlanningProject/src/main.cpp
@@ -244,42 +244,52 @@ int main() {
 			vector<double> next_x_vals;
 			vector<double> next_y_vals;
 
-
-			// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
-			double pos_x, pos_y;
-			double angle;
-			int path_size = previous_path_x.size();
-			assert(previous_path_x.size() == previous_path_y.size());
-
-			for (int i = 0; i < path_size; i ++)
-			{
-				next_x_vals.push_back(previous_path_x[i]);
-				next_y_vals.push_back(previous_path_y[i]);
-			}
-
-			if (path_size == 0)
-			{
-				pos_x = car_x;
-				pos_y = car_y;
-				angle = deg2rad(car_yaw);
-			}
-			else
-			{
-				pos_x = previous_path_x[path_size - 1];
-				pos_y = previous_path_y[path_size - 1];
-				double pos_x2 = previous_path_x[path_size - 2];
-				double pos_y2 = previous_path_y[path_size - 2];
-				angle = atan2(pos_y - pos_y2, pos_x - pos_x2);
-			}
-
 			double dist_inc = 0.5;
-			for (int i = 0; i < 50 - path_size; i ++)
+			for (int i = 0; i < 50; i ++)
 			{
-				next_x_vals.push_back(pos_x + dist_inc * cos(angle + (i + 1) * pi() / 100));
-				next_y_vals.push_back(pos_y + dist_inc * sin(angle + (i + 1) * pi() / 100));
-				pos_x += dist_inc * cos(angle + (i + 1) * pi() / 100);
-				pos_y += dist_inc * sin(angle + (i + 1) * pi() / 100);
+				double next_s = car_x + (i + 1) * dist_inc;
+				double next_d = 6.;
+				vector<double> next_xy = getXY(next_s, next_d, maps_s, maps_x, maps_y);
+				double next_x = next_xy[0];
+				double next_y = next_xy[1];
+				next_x_vals.push_back(next_x);
+				next_y_vals.push_back(next_y);
 			}
+			// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+			// double pos_x, pos_y;
+			// double angle;
+			// int path_size = previous_path_x.size();
+			// assert(previous_path_x.size() == previous_path_y.size());
+
+			// for (int i = 0; i < path_size; i ++)
+			// {
+			// 	next_x_vals.push_back(previous_path_x[i]);
+			// 	next_y_vals.push_back(previous_path_y[i]);
+			// }
+
+			// if (path_size == 0)
+			// {
+			// 	pos_x = car_x;
+			// 	pos_y = car_y;
+			// 	angle = deg2rad(car_yaw);
+			// }
+			// else
+			// {
+			// 	pos_x = previous_path_x[path_size - 1];
+			// 	pos_y = previous_path_y[path_size - 1];
+			// 	double pos_x2 = previous_path_x[path_size - 2];
+			// 	double pos_y2 = previous_path_y[path_size - 2];
+			// 	angle = atan2(pos_y - pos_y2, pos_x - pos_x2);
+			// }
+
+			// double dist_inc = 0.5;
+			// for (int i = 0; i < 50 - path_size; i ++)
+			// {
+			// 	next_x_vals.push_back(pos_x + dist_inc * cos(angle + (i + 1) * pi() / 100));
+			// 	next_y_vals.push_back(pos_y + dist_inc * sin(angle + (i + 1) * pi() / 100));
+			// 	pos_x += dist_inc * cos(angle + (i + 1) * pi() / 100);
+			// 	pos_y += dist_inc * sin(angle + (i + 1) * pi() / 100);
+			// }
 
 
 			// double dist_inc = 0.5;
