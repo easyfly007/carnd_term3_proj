cda685cf1b678a5938099f4319daaa00d109f8d4
easyfly007 <easyfly.huang@gmail.com>
2018-04-15 13:18:19 +0800

    add optimizer code

--- SemanticSegmentationProject/main.py
+++ SemanticSegmentationProject/main.py
@@ -103,7 +103,15 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+    print('nn_last_layer shape = ', nn_last_layer.shape)
+    print('correct_label shape = ', correct_label.shape)
+    correct_label_reshaped = tf.reshape(correct_label, (-1, num_classes))
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    	labels =correct_label_reshaped, logits = logits))
+    train_op = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(cross_entropy_loss)
+    return logits, train_op, cross_entropy_loss
+
 tests.test_optimize(optimize)
 
 
