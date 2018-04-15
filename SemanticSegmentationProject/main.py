0f2da5db762bd8ed92bfbbabc6c90168080260dd
easyfly007 <easyfly.huang@gmail.com>
2018-04-15 14:17:09 +0800

    add vgg train code

--- SemanticSegmentationProject/main.py
+++ SemanticSegmentationProject/main.py
@@ -33,6 +33,7 @@ def load_vgg(sess, vgg_path):
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
 
+    print('load vgg file from vgg_path =', vgg_path)
     tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
     graph = tf.get_default_graph()
     vgg_input = graph.get_tensor_by_name(vgg_input_tensor_name)
@@ -131,9 +132,16 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
-    pass
-tests.test_train_nn(train_nn)
+    print('epochs = ', epochs)
+    for epoch in range(epochs):
+    	for labels, images in get_batches_fn(batch_size):
+    		sess.run(train_op, feed_dict = {
+    			input_image: images,
+    			correct_label: labels,
+    			keep_prob: 0.5,
+    			learning_rate: 0.001 })
 
+tests.test_train_nn(train_nn)
 
 def run():
     num_classes = 2
@@ -152,6 +160,8 @@ def run():
     with tf.Session() as sess:
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
+        # vgg_path = os.path.join(data_dir, '')
+        print('vgg_path = ', vgg_path)
         # Create function to get batches
         get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
 
@@ -159,14 +169,22 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
+        vgg_input, vgg_keepprob, vgg_layer3_out, vgg_layer4_out, vgg_layer7_out = load_vgg(sess, vgg_path)
+        output = layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes)
+        logits, train_op, cross_entropy_loss = optimize(output, correct_label, learning_rate, num_classes)
 
         # TODO: Train NN using the train_nn function
+        # train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
+        #            correct_label, keep_prob, learning_rate):
+        train_nn(sess, epochs, 1024, get_batches_fn, train_op, cross_entropy_loss, vgg_input,
+        	correct_label, keep_prob, learning_rate)
+
 
         # TODO: Save inference data using helper.save_inference_samples
         #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
-
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
         # OPTIONAL: Apply the trained model to a video
-
+        
 
 if __name__ == '__main__':
     run()
